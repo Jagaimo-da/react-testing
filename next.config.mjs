30bfaecb402c735d20a756f32304b0938d548938
OptrRiri <roy.oliver.yeo@gmail.com>
2024-07-22 20:30:58 +0900

    send

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/react-testing',
-    assetPrefix: '/react-testing/'
+    assetPrefix: '/react-testing/',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
